4b9402348434111cfe12c78ba03843ced8cef339
jccaychop <juan.caycho.paucar@outlook.com>
2023-08-17 23:35:42 -0500

    Avance de grow navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import './styles.css'
 
 export const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => setIsOpen(!isOpen);
+  const [isDivExpanded, setIsDivExpanded] = useState(false);
+
+  const toggleDiv = () => {
+    const growDiv = document.getElementById('grow');
+    const wrapper = document.querySelector('.measuringWrapper');
+
+    if (growDiv && growDiv.clientHeight) {
+      growDiv.style.height = '0';
+      setIsDivExpanded(false);
+    } else if(growDiv && wrapper){
+      growDiv.style.height = `${wrapper.clientHeight}px`;
+      setIsDivExpanded(true);
+    }
+  };
+
+  // const handleClick = () => setIsOpen(!isOpen);
+  const handleClick = () => toggleDiv();
+
+  console.log({isDivExpanded});
+  
 
   return (
-    <div className="bg-space-cadet fixed w-full z-20 md:h-16">
-      <div className="">
+    <div className="bg-space-cadet sticky top-0 w-full z-20 md:h-16 border border-[red]">
+      <div className="h-full">
         <nav className="relative bg-transparent md:h-full m-0 mx-auto flex flex-col max-w-[73.125rem] text-white uppercase font-Roboto">
           <div
             className="bg-space-cadet md:hidden font-bold text-3xl cursor-pointer z-30"
-            onClick={handleClick}
+            onClick={()=> handleClick()}
           >
             <p className="text-center leading-[1.625rem] py-4 px-0">menu</p>
           </div>
 
-          <div
-            className={`absolute top-14 w-full bg-space-cadet ease-in-out duration-700 ${
-              isOpen ? "translate-y-0" : "-translate-y-full"
-            }`}
+          {/* <div
+            className={`absolute top-14 md:top-0 w-full bg-space-cadet delay-300 ${
+              isOpen ? "h-auto" : "h-0 hidden"
+            } md:block border border-white md:h-full`}
+          > */}
+          <div id="grow"
+            className="absolute w-full top-14 bg-space-cadet md:block border border-white h-auto md:h-full"
           >
-
-            <ul className="bg-transparent md:h-full max-w-xs md:max-w-none w-full m-0 mx-auto text-center">
+            <ul className="measuringWrapper bg-transparent md:h-full max-w-xs md:max-w-none w-full m-0 mx-auto text-center">
+            {/* <ul className="bg-transparent max-w-xs md:max-w-none w-full m-0 mx-auto text-center"> */}
               <li className="md:h-full md:inline-flex md:items-center text-[13px] leading-6 tracking-[0.07rem] py-3 px-0 border-b md:border-none last:border-none">
                 <a href="/#home" className="block md:p-3 md:mx-1">
                   home
